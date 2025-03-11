1ee24208678a313c57dc13e8bf890eac71b1bff3
Franz Macalua <franzylbjornlmacalua@su.edu.ph>
2025-03-11 08:05:58 +0800

    Add files via upload

--- MacaluaCP4a.c
+++ MacaluaCP4a.c
@@ -1,11 +1,19 @@
+// Franzyl Bjorn L. Macalua
+/* Problem is to use 2d arrays to record the test scores of 10 students. 
+Storing them in one array and then calculating their statistics and showing
+the 2d arrays in a table */
+// Date Created: 3/10/2025
+// Date Finished: 3/11/2025
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 
-#define ROW 5
+#define ROW 10
 #define COLUMN 6
 #define STATCOLUMN 3
 
+void dataInput(int record[][COLUMN]);
 void output(int record[][COLUMN], int statistics[][STATCOLUMN]);
 void lowScore(int record[][COLUMN], int statistics[][STATCOLUMN]);
 void highScore(int record[][COLUMN], int statistics[][STATCOLUMN]);
@@ -14,18 +22,40 @@ void averageScore(int record[][COLUMN], int statistics[][STATCOLUMN]);
 int main() {
     int record[ROW][COLUMN];
     int statistics[ROW][STATCOLUMN];
+    
+    dataInput(record);
+    lowScore(record, statistics);
+    highScore(record, statistics);
+    averageScore(record, statistics);
+    output(record, statistics);
+}
 
+void dataInput(int record[][COLUMN]) {
     for (int i = 0; i < ROW; i++) {
         int studentID;
         do {
-            printf("Student ID: ");
+        	int duplicateCheck = 0;
+            printf("%d Student ID: ", i + 1);
             scanf("%d", &studentID);
+            
+            for (int search = 0; search < ROW; search++) {
+				if (studentID == record[i][0]) {
+					duplicateCheck = 1;
+					break;
+				}
+			}
             if (studentID < 1000 || studentID > 9999) {
                 printf("Out of range! (Please select ID between 1000-9999)\n");
                 getch();
                 system("cls");
             }
-        } while (studentID < 1000 || studentID > 9999);
+            else if (duplicateCheck == 1) {
+				printf("Already an existing student code!");
+				getch();
+				system("cls");
+		}
+        } while (studentID < 1000 || studentID > 9999 || duplicateCheck == 1);
+        
         record[i][0] = studentID;
 
         for (int j = 1; j < COLUMN; j++) {
@@ -42,10 +72,6 @@ int main() {
             record[i][j] = quizScore;
         }
     }
-    lowScore(record, statistics);
-    highScore(record, statistics);
-    averageScore(record, statistics);
-    output(record, statistics);
 }
 
 void output(int record[][COLUMN], int statistics[][STATCOLUMN]) {
@@ -67,9 +93,9 @@ void lowScore(int record[][COLUMN], int statistics[][STATCOLUMN]) {
     for (int i = 0; i < ROW; i++) {
         int smallest = record[i][1];
 
-        for (int j = 2; i < (COLUMN - 1); j++) {  // Start from the second element
+        for (int j = 2; j < (COLUMN - 1); j++) { 
             if (record[i][j] < smallest) {
-                smallest = record[i][j];  // Update largest if a larger element is found
+                smallest = record[i][j]; 
             }
         }
         statistics[i][1] = smallest;
@@ -80,9 +106,9 @@ void highScore(int record[][COLUMN], int statistics[][STATCOLUMN]) {
     for (int i = 0; i < ROW; i++) {
         int largest = record[i][1];
 
-        for (int j = 2; i < (COLUMN - 1); j++) {  // Start from the second element
+        for (int j = 2; j < (COLUMN - 1); j++) { 
             if (record[i][j] > largest) {
-                largest = record[i][j];  // Update largest if a larger element is found
+                largest = record[i][j]; 
             }
         }
         statistics[i][0] = largest;
@@ -93,8 +119,7 @@ void averageScore(int record[][COLUMN], int statistics[][STATCOLUMN]) {
     for (int i = 0; i < ROW; i++){
         int sum = 0;
         for (int j = 1; j < COLUMN; j++) {
-            sum = record[i][j];;
-            sum += record[i][j + 1];
+            sum += record[i][j];
         }
         statistics[i][2] = sum / (COLUMN - 1);
     }
